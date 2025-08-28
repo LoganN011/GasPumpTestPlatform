1e8657694196514ee2402b68ceb19a96ccac6475
quinn <gsena@unm.edu>
2025-08-27 19:24:23 -0600

    Created packages and basic interface test

--- src/Sockets/Main.java
+++ src/Sockets/Main.java
@@ -1,3 +1,5 @@
+import Sockets.Client;
+
 public class Main {
 
     public static void main(String[] args) {
---
+++ src/UI/Card.java
@@ -0,0 +1,47 @@
+package UI;
+
+import javafx.application.Application;
+import javafx.scene.Scene;
+import javafx.scene.control.Button;
+import javafx.scene.control.Label;
+import javafx.scene.layout.*;
+import javafx.scene.paint.Color;
+import javafx.scene.paint.Paint;
+import javafx.stage.Stage;
+
+import Sockets.*;
+
+public class Card extends Application {
+
+
+    public static void main(String[] args) {
+        launch(args);
+    }
+
+    @Override
+    public void start(Stage primaryStage) {
+        //todo replace this with an instance of API
+        Client self = new Client(1234);
+        self.start();
+
+        Button test = new Button();
+        test.setMinSize(100, 100);
+        test.setOnMouseClicked(x ->{
+            String cardNumber = "";
+            while (cardNumber.length() < 20) cardNumber += (int)(Math.random() * 10);
+            System.out.println(cardNumber);
+
+            //todo relay message through communication API containing number
+            //  may look like this:
+            //self.sendMessage(cardNumber);
+        });
+
+        HBox root = new HBox();
+        root.getChildren().add(test);
+
+        Scene scene = new Scene(root);
+        primaryStage.setScene(scene);
+        primaryStage.setTitle("Gas Nozzle");
+        primaryStage.show();
+    }
+}
