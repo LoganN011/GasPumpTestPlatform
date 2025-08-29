2907a5be461ca7952dda60fa66debaec6d1d19b7
quinn <gsena@unm.edu>
2025-08-28 20:56:26 -0600

    Layed out expected use in Harness.java

--- src/Devices/Harness.java
+++ src/Devices/Harness.java
@@ -2,6 +2,12 @@ package Devices;
 
 public class Harness {
     public static void main(String[] args) {
-        //TODO
+        //Format for messages to screen:
+        //  Buttons:    b:n:t               where n is the button number, and t is x for exclusive and m for multiple
+        //  Text:       t:n[n]:sx:fy:cz:m   where n[n] is the text number, x is the size to use, y is the font to use, z is the color to use, and m is the message
+        //String testScreenOne = "b:1:x,t:01:s1:f1:c1:Hello World"
+
+        //commPort screenComm = new commPort("screen");
+        //screenComm.sendMessage();
     }
 }
