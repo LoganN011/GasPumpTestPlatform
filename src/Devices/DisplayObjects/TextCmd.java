aae59a12cc4804edc9b7b1121490d0585637450b
Ryan Hossain <notryan10@unm.edu>
2025-09-04 04:31:20 -0600

    Added message markup

---
+++ src/Devices/DisplayObjects/TextCmd.java
@@ -0,0 +1,22 @@
+package Devices.GUI;
+
+import javafx.scene.text.Text;
+
+public class TextCmd {
+    public final String pair;
+    public final int s, f, c;
+    public final boolean split;
+    public final String left, right, text;
+    public final Text styledFromMR; // copy font/fill from MR text node
+    public TextCmd (String pair, int s, int f, int c, boolean split, String left, String right, String text, Text styledFromMR) {
+        this.pair = pair;
+        this.s = s;
+        this.f = f;
+        this.c = c;
+        this.split = split;
+        this.left = left;
+        this.right = right;
+        this.text = text;
+        this.styledFromMR = styledFromMR;
+    }
+}
