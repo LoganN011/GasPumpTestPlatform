4f56c602c5ce26343cba8141d1853984a67d007b
lnunno1 <lnunno1@unm.edu>
2025-09-05 13:40:23 -0600

    working dragging and dropping for connecting to the car

--- src/Devices/Hose.java
+++ src/Devices/Hose.java
@@ -5,8 +5,12 @@ import Sockets.controlPort;
 import javafx.application.Application;
 import javafx.scene.Scene;
 import javafx.scene.control.Button;
+import javafx.scene.control.ProgressBar;
 import javafx.scene.layout.BorderPane;
 import javafx.scene.layout.GridPane;
+import javafx.scene.layout.Pane;
+import javafx.scene.shape.Circle;
+import javafx.scene.shape.Rectangle;
 import javafx.stage.Stage;
 
 import java.io.IOException;
@@ -14,6 +18,7 @@ import java.io.IOException;
 public class Hose extends Application {
 
     private int gasTank;
+    private boolean connected;
 
     public static void main(String[] args) {
         launch(args);
@@ -44,12 +49,61 @@ public class Hose extends Application {
                 throw new RuntimeException(ex);
             }
         });
-        BorderPane root = new BorderPane();
-        root.setPrefSize(200,200);
-        root.setCenter(button);
+//        ProgressBar gasTank = new ProgressBar();
+//        gasTank.setProgress((Math.random()));
+//        gasTank.setStyle("-fx-accent: yellow;");
+//        gasTank.setRotate(90);
+
+        Circle pumpHandle = new Circle(25);
+        pumpHandle.setCenterX(100);
+        pumpHandle.setCenterY(150);
+
+
+        pumpHandle.setOnMouseDragged(e ->{
+            pumpHandle.setCenterX(e.getX());
+            pumpHandle.setCenterY(e.getY());
+
+        });
+        pumpHandle.setOnMouseReleased(e -> {
+            try{
+                if(e.getX() >= 700) {
+                    System.out.println("on car");
+                    connected = true;
+                    harness.send(new Message("connected"));
+                    pumpHandle.setCenterX(700);
+                    pumpHandle.setCenterY(200);
+                }
+                else {
+                    if(connected) {
+                        connected = false;
+                        harness.send(new Message("disconnected"));
+                    }
+
+                }
+            }catch(Exception ex){
+                ex.printStackTrace();
+            }
+
+        });
+
+        Rectangle pump = new Rectangle(100,400);
+        pump.setStyle("-fx-fill: #FF0000");
+
+
+        Rectangle car = new Rectangle(100,400);
+        car.setStyle("-fx-fill: grey");
+        car.setX(700);
+
+        Pane root = new Pane();
+        root.setPrefSize(800,400);
+        root.getChildren().addAll(pump,car,pumpHandle);
+
+
+
 
 
         Scene scene = new Scene(root);
+
         primaryStage.setScene(scene);
         primaryStage.setTitle("Hose");
         primaryStage.show();
