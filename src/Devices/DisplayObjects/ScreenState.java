1e6ac97118bcad853e5bc556125734646e61ff6f
Ryan Hossain <notryan10@unm.edu>
2025-09-15 02:42:06 -0600

    Added Display screen states

---
+++ src/Devices/DisplayObjects/ScreenState.java
@@ -0,0 +1,58 @@
+package Devices.DisplayObjects;
+
+import Message.Message;
+import Sockets.commPort;
+
+import java.io.IOException;
+
+public class ScreenState {
+
+    /**
+     * Welcome screen (idle)
+     */
+    public static void welcomeScreen(commPort device) throws IOException {
+        device.send(new Message("t:01:s0:f0:c2:WELCOME!"));
+        device.send(new Message("t:45:s1:f1:c1:Please tap your credit card or phone's digital card to begin."));
+        device.send(new Message("b:8:x,b:9:x,t:89:s2:f2:c0:BEGIN|CANCEL"));
+    }
+
+    /**
+     * Fuel selection screen
+     */
+    public static void fuelSelectionScreen(commPort device) throws IOException {
+        device.send(new Message("t:01:s0:f0:c2:SELECT YOUR GAS TYPE"));
+        device.send(new Message("b:2:m,b:3:m,t:23:s1:f1:c1:REGULAR 87"));
+        device.send(new Message("b:4:m,b:5:m,t:45:s1:f1:c1:PLUS 89"));
+        device.send(new Message("b:6:m,b:7:m,t:67:s1:f1:c1:PREMIUM 91"));
+        device.send(new Message("b:8:x,b:9:x,t:89:s2:f2:c0:BEGIN FUELING|CANCEL"));
+    }
+
+    /**
+     * Payment failure screen
+     */
+    public static void paymentDeclinedScreen(commPort device) throws IOException {
+        device.send(new Message("t:01:s0:f0:c2:PAYMENT FAILURE"));
+        device.send(new Message("t:23:s1:f1:c1:Payment was declined."));
+        device.send(new Message("b:8:x,b:9:x,t:89:s2:f2:c0:|OK"));
+    }
+
+    /**
+     * Currently pumping fuel screen
+     */
+    // "꞉" is a usable colon that won't get caught by MessageReader
+    public static void pumpingScreen(commPort device) throws IOException {
+        device.send(new Message("t:01:s0:f0:c2:PUMPING IN PROGRESS"));
+        device.send(new Message("t:23:s2:f1:c1:Gallons꞉ " + 10));
+        device.send(new Message("t:45:s2:f1:c1:Price꞉ $" + 9));
+        device.send(new Message("b:8:x,b:9:x,t:89:s2:f2:c0:PAUSE|EXIT"));
+    }
+
+    /**
+     * Finished pumping screen
+     */
+    public static void finishScreen(commPort device) throws IOException {
+        device.send(new Message("t:01:s0:f0:c2:PUMPING FINISHED"));
+        device.send(new Message("t:23:s1:f1:c1:Thank you for refilling with us!"));
+        device.send(new Message("b:8:x,b:9:x,t:89:s2:f2:c0:|OK"));
+    }
+}
