545c73e6da95a7af76ce0bed98d40a05ff7c28de
lnunno1 <lnunno1@unm.edu>
2025-09-19 15:16:31 -0600

    added basic idea for the fueling process

--- src/Controller/Fueling.java
+++ src/Controller/Fueling.java
@@ -1,16 +1,53 @@
 package Controller;
 
 public class Fueling extends Thread{
-
     private Hose hose;
     private PumpingAssembly pumpingAssembly;
 
     public Fueling(){
         hose = new Hose();
         pumpingAssembly = new PumpingAssembly();
+        start();
     }
 
+    //Am I missing anything?
     public void run(){
-        //DO something is here based on state
+        while(true){
+            switch (Controller.getState()){
+                case ATTACHING, DETACHED -> {
+                    if(hose.isAttached()){
+                        Controller.setState(InternalState.FUELING);
+                    }
+                }
+                case FUELING -> {
+                    if(hose.isFull()){
+                        Controller.setState(InternalState.DETACHING);
+                        pumpingAssembly.pumpOff();
+                    }
+                    else if (!hose.isAttached()){
+                        pumpingAssembly.pumpOff();
+                        Controller.setState(InternalState.DETACHED);
+                    }
+                    else {
+                        pumpingAssembly.pumpOn("TYPE"); //Get the type somehow
+                        //return flow somehow with
+                        System.out.println(pumpingAssembly.readFlow());
+                    }
+                }
+                case PAUSED -> {
+                    pumpingAssembly.pumpOff();
+
+                }
+                case DETACHING -> {
+                    if(!hose.isAttached()){
+                        Controller.setState(InternalState.COMPLETE);
+                    }
+                }
+                case OFF,COMPLETE -> {
+                    pumpingAssembly.pumpOff();
+                    pumpingAssembly.resetFlow();
+                }
+            }
+        }
     }
 }
