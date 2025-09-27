0ff4ca0d3f2bac1bc36bbee96411604bd98a070c
lnunno1 <lnunno1@unm.edu>
2025-09-26 20:44:54 -0600

    Added timer to fueling process

--- src/Controller/Fueling.java
+++ src/Controller/Fueling.java
@@ -9,7 +9,13 @@ public class Fueling {
         new Thread(() -> {
             while (true) {
                 switch (Controller.getState()) {
-                    case ATTACHING, DETACHED -> {
+                    case ATTACHING -> {
+                        if (hose.isAttached()) {
+                            Controller.setState(InternalState.FUELING);
+                        }
+                    }
+                    case DETACHED ->{
+                        Controller.setTimer(10);
                         if (hose.isAttached()) {
                             Controller.setState(InternalState.FUELING);
                         }
@@ -18,9 +24,11 @@ public class Fueling {
                         if (hose.isFull()) {
                             Controller.setState(InternalState.DETACHING);
                             pump.pumpOff();
+                            Controller.setTimer(10);
                         } else if (!hose.isAttached()) {
                             pump.pumpOff();
                             Controller.setState(InternalState.DETACHED);
+                            Controller.setTimer(10);
                         } else {
                             pump.pumpOn(Controller.getCurrentGas().getName());
                             Controller.setGasAmount(Integer.parseInt(flowMeter.readFlow().toString()));
@@ -28,11 +36,12 @@ public class Fueling {
                     }
                     case PAUSED -> {
                         pump.pumpOff();
-
+                        Controller.setTimer(10);
                     }
                     case DETACHING -> {
                         if (!hose.isAttached()) {
                             Controller.setState(InternalState.COMPLETE);
+                            Controller.setTimer(10);
                         }
                     }
                     case OFF, COMPLETE -> {
