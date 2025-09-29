785ceb365976384b89d4994657546ce7967d89dd
Georgina Personal <ganchondogaribaldi99@unm.edu>
2025-09-29 03:39:42 -0600

    updated GUI for pump and hose

--- src/Devices/HoseGUI.java
+++ src/Devices/HoseGUI.java
@@ -8,7 +8,8 @@ import javafx.application.Application;
 import javafx.scene.Scene;
 import javafx.scene.control.Button;
 import javafx.scene.control.ProgressBar;
-import javafx.scene.layout.Pane;
+import javafx.scene.image.Image;
+import javafx.scene.layout.*;
 import javafx.scene.shape.Circle;
 import javafx.scene.shape.Line;
 import javafx.scene.shape.QuadCurve;
@@ -20,7 +21,6 @@ public class HoseGUI extends Application {
 
     private boolean connected;
     private boolean full;
-    private final double SIZE= 400;
 
     public static void main(String[] args) {
         launch(args);
@@ -35,15 +35,24 @@ public class HoseGUI extends Application {
             e.printStackTrace();
         }
 
+        Pane root = new Pane();
+        Scene scene = new Scene(root, 500,500);
+
+        double SIZE = 400;
+        double rootHeight = root.getHeight();
+        double rootWidth = root.getWidth();
+        double sceneW = scene.getHeight();
+        double sceneH = scene.getWidth();
+
         controlPort harness = control;
         ProgressBar gasTank = new ProgressBar();
         gasTank.setProgress((Math.random()));
         gasTank.setStyle("-fx-accent: yellow;-fx-control-inner-background: black;");
         gasTank.setRotate(-90);
-        gasTank.setLayoutX((SIZE - (SIZE/8))+(SIZE/32));
-        gasTank.setLayoutY(SIZE/4);
+        gasTank.setLayoutX((rootWidth - (rootWidth/8))+(rootWidth/32));
+        gasTank.setLayoutY(rootHeight/4);
         //resize gas tank
-        gasTank.setPrefWidth(SIZE/8);
+        gasTank.setPrefWidth(rootWidth/8);
 
 
         Button clear = new Button("Empty");
@@ -79,12 +88,12 @@ public class HoseGUI extends Application {
         animation.setCycleCount(Timeline.INDEFINITE);
 
         Circle pumpHandle = new Circle(SIZE/32);
-        pumpHandle.setCenterX(SIZE/8);
-        pumpHandle.setCenterY(SIZE/5);
+        pumpHandle.setCenterX(sceneW/7);
+        pumpHandle.setCenterY(sceneH/2);
 
         Line hoseLine = new Line();
-        hoseLine.setStartX(SIZE/8);
-        hoseLine.setStartY(SIZE/32);
+        hoseLine.setStartX(sceneW/7);
+        hoseLine.setStartY(sceneH/7);
         hoseLine.setEndX(pumpHandle.getCenterX());
         hoseLine.setEndY(pumpHandle.getCenterY());
         hoseLine.setStrokeWidth(2);
@@ -97,7 +106,7 @@ public class HoseGUI extends Application {
             hoseLine.setEndY(e.getY());
         });
         pumpHandle.setOnMouseReleased(e -> {
-            if (e.getX() >= (SIZE - (SIZE/8))) {
+            if (e.getX() >= (sceneW - (sceneW/2))) {
                 System.out.println("on car");
                 connected = true;
                 if (full) {
@@ -106,40 +115,52 @@ public class HoseGUI extends Application {
                     harness.send(new Message("connected"));
                 }
 
-                pumpHandle.setCenterX(SIZE - (SIZE/8));
-                pumpHandle.setCenterY(SIZE/4);
+                pumpHandle.setCenterX(sceneW - (sceneW/2));
+                pumpHandle.setCenterY(sceneH/4);
                 animation.playFromStart();
-                hoseLine.setEndX(SIZE - (SIZE/8));
-                hoseLine.setEndY(SIZE/4);
+                hoseLine.setEndX(sceneW - (sceneW/2));
+                hoseLine.setEndY(sceneH/4);
             } else {
                 if (connected) {
                     animation.stop();
                     connected = false;
                     harness.send(new Message("disconnected"));
-                    pumpHandle.setCenterX(SIZE/8);
-                    pumpHandle.setCenterY(SIZE/5);
-                    hoseLine.setEndX(SIZE/8);
-                    hoseLine.setEndY(SIZE/5);
+                    pumpHandle.setCenterX(sceneW/7);
+                    pumpHandle.setCenterY(sceneH/2);
+                    hoseLine.setEndX(pumpHandle.getCenterX());
+                    hoseLine.setEndY(pumpHandle.getCenterY());
                 }
 
             }
         });
 
-        Rectangle pump = new Rectangle(SIZE/8, SIZE/2);
-        pump.setStyle("-fx-fill: #FF0000");
+//        Rectangle pump = new Rectangle(rootWidth/8, rootHeight/2);
+//        pump.setStyle("-fx-fill: #FF0000");
 
 
-        Rectangle car = new Rectangle(SIZE/8, SIZE/2);
-        car.setStyle("-fx-fill: grey");
-        car.setX(SIZE - (SIZE/8));
+//        Rectangle car = new Rectangle(SIZE/8, SIZE/2);
+//        car.setStyle("-fx-fill: grey");
+//        car.setX(SIZE - (SIZE/8));
 
-        Pane root = new Pane();
+        Image backgroundImage = VisualElements.getImage("hose.png");
+
+        BackgroundImage background = new BackgroundImage(backgroundImage, BackgroundRepeat.NO_REPEAT, BackgroundRepeat.NO_REPEAT, BackgroundPosition.CENTER, new BackgroundSize(
+                100,
+                100,
+                true,
+                true,
+                false,
+                true
+        ));
+
+
         root.setPrefSize(SIZE, SIZE/2);
-        root.getChildren().addAll(pump, car,hoseLine, pumpHandle, gasTank, clear);
+        root.getChildren().addAll(hoseLine, pumpHandle, gasTank, clear);
 //        root.getChildren().addAll(pump, car, pumpHandle, gasTank, clear);
+        root.setBackground(new Background(background));
 
 
-        Scene scene = new Scene(root);
+
 
         primaryStage.setScene(scene);
         primaryStage.setTitle("Hose");
--- src/Devices/PumpGUI.java
+++ src/Devices/PumpGUI.java
@@ -7,13 +7,12 @@ import javafx.animation.RotateTransition;
 import javafx.application.Application;
 import javafx.application.Platform;
 import javafx.geometry.Insets;
+import javafx.geometry.Pos;
 import javafx.scene.Scene;
 import javafx.scene.control.Label;
 import javafx.scene.image.Image;
 import javafx.scene.image.ImageView;
-import javafx.scene.layout.BorderPane;
-import javafx.scene.layout.StackPane;
-import javafx.scene.layout.VBox;
+import javafx.scene.layout.*;
 import javafx.scene.paint.Color;
 import javafx.scene.shape.Circle;
 import javafx.scene.shape.Rectangle;
@@ -49,24 +48,38 @@ public class PumpGUI extends Application {
         } catch (Exception e) {
             e.printStackTrace();
         }
+        BorderPane root = new BorderPane();
 
         pumpImage = VisualElements.getImage("off.png", (int) (SIZE/2));
-        pumpImage.setFitHeight(SIZE/2);
-        pumpImage.setFitWidth(SIZE/2);
+        pumpImage.setPreserveRatio(true);
+        pumpImage.fitWidthProperty().bind(root.widthProperty().multiply(0.5));
+        pumpImage.fitHeightProperty().bind(root.heightProperty().multiply(0.5));
 
         StackPane flowMeter = new StackPane();
         gauge = new Circle(SIZE/8);
         gauge.setFill(Color.WHITE);
         gauge.setStroke(Color.BLACK);
-        line = new Rectangle(SIZE/40, SIZE/4);
-        Rectangle background = new Rectangle(SIZE/3.2,SIZE/3.2);
+        gauge.radiusProperty().bind(root.widthProperty().multiply(0.05));
 
-        flowMeter.getChildren().addAll(background,gauge, line);
+        line = new Rectangle();
+
+        line.widthProperty().bind(root.widthProperty().multiply(0.01));
+        line.heightProperty().bind(root.heightProperty().multiply(0.15));
+
+        Rectangle flowBackground = new Rectangle();
+
+        flowBackground.widthProperty().bind(root.widthProperty().multiply(0.25));
+        flowBackground.heightProperty().bind(root.heightProperty().multiply(0.25));
+
+        flowMeter.getChildren().addAll(flowBackground,gauge, line);
         VBox flowAssembly = new VBox();
 
         flowCounter = new Label("0");
 
-        flowCounter.setFont(new Font("Lucida Console",SIZE/16));
+        root.heightProperty().addListener((obs, oldVal, newVal) -> {
+            flowCounter.setFont(new Font("Lucida Console", newVal.doubleValue() / 20));
+        });
+
         flowCounter.setStyle(
                 "-fx-text-fill: red;" +
                         "-fx-background-color: black;" +
@@ -75,8 +88,13 @@ public class PumpGUI extends Application {
                         "-fx-border-width: SIZE/200;" +
                         "-fx-border-radius: SIZE/100;"
         );
-        Rectangle textBackground = new Rectangle(SIZE/3.2,SIZE/8);
+        Rectangle textBackground = new Rectangle();
+        textBackground.widthProperty().bind(root.widthProperty().multiply(0.25));
+        textBackground.heightProperty().bind(root.heightProperty().multiply(0.1));
+
         flowAssembly.getChildren().addAll(new StackPane(textBackground,flowCounter), flowMeter);
+        flowAssembly.prefWidthProperty().bind(root.widthProperty());
+        flowAssembly.prefHeightProperty().bind(root.heightProperty().multiply(0.6));
 
         animation = new RotateTransition(Duration.millis(250), line);
         animation.setByAngle(360);
@@ -90,14 +108,29 @@ public class PumpGUI extends Application {
             }
         });
 
+        Image backgroundImage = VisualElements.getImage("pump_background.png");
 
-        BorderPane root = new BorderPane();
+        BackgroundImage background = new BackgroundImage(backgroundImage, BackgroundRepeat.NO_REPEAT, BackgroundRepeat.NO_REPEAT, BackgroundPosition.CENTER, new BackgroundSize(
+                100,
+                100,
+                true,
+                true,
+                false,
+                true
+        ));
+
+
         root.setPadding(new Insets(10));
         root.setMinSize(SIZE, SIZE);
-        root.setCenter(pumpImage);
-        root.setRight(flowAssembly);
+        root.setBottom(pumpImage);
+        BorderPane.setAlignment(pumpImage, Pos.CENTER);
+        root.setTop(flowAssembly);
+        root.setBackground(new Background(background));
+        flowAssembly.prefWidthProperty().bind(root.widthProperty());
+        flowAssembly.prefHeightProperty().bind(root.heightProperty().multiply(0.8));
 
-        Scene scene = new Scene(root);
+        Scene scene = new Scene(root, 500, 500);
+
         primaryStage.setScene(scene);
         primaryStage.setTitle("Pump Assembly");
         primaryStage.show();
