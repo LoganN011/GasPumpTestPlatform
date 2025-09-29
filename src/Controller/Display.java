5e45b083cadc4c4bc7fe6f03e05ea130fd046da7
quinn <gsena@unm.edu>
2025-09-29 13:13:25 -0600

    display has a thread now

--- src/Controller/Display.java
+++ src/Controller/Display.java
@@ -10,47 +10,62 @@ import static Controller.InternalState.*;
 
 public class Display {
 
-    private commPort device;
+    private static commPort device;
 
     //todo: remove once displayGUI code properly removes old texts and shows all new messages independently
     private static InternalState lastState = DETACHING;
 
-    public void start() {
-        device = new commPort("screen");
-        while (true) {
-            switch (Controller.getState()) {
-                case OFF, STANDBY -> pumpUnavailable();
-                case IDLE -> welcome();
-                case AUTHORIZING -> authorizing();
-                case SELECTION -> fuelSelect();
-                case DECLINED -> cardDeclined();
-                case ATTACHING -> attachHose();
-                case FUELING -> fueling();
-                case DETACHED -> detached();
-//                case PAUSED -> paused();
+    public static void start() {
+        new Thread(() -> {
+            device = new commPort("screen");
+            while (true) {
+                switch (Controller.getState()) {
+                    case OFF, STANDBY -> pumpUnavailable();
+                    case IDLE -> welcome();
+                    case AUTHORIZING -> authorizing();
+                    case SELECTION -> fuelSelect();
+                    case DECLINED -> cardDeclined();
+                    case ATTACHING -> attachHose();
+                    case FUELING -> fueling();
+                    case DETACHED -> detached();
+                    case PAUSED -> paused();
 //                case DETACHING -> detaching();
 //                case COMPLETE -> complete();
 //                case OFF_DETACHING -> detaching();
-                //TODO add the remainder of the states
+                    //TODO add the remainder of the states
+                }
             }
-        }
+        }).start();
+
     }
 
-    private void detached() {
+    private static void paused() {
+        String message = "";
+        message += "t:01:s0:f0:c2:FUELING PAUSED";
+        message += String.format(",t:23:s2:f1:c1:Gallons꞉ %d", Controller.getGasAmount());
+        message += String.format(",t:45:s2:f1:c1:Price꞉ $%.2f", Controller.getCurPrice());
+        message += ",t:67:s1:f1:c1:Press RESUME to continue or FINISH";
+        message += ",t:89:s0:f0:c2: ";
+        message += ",b:8:x,b:9:x,t:89:s2:f2:c0:RESUME|FINISH";
+        device.send(new Message(message));
+    }
+
+    private static void detached() {
         String message = "";
         message += "t:01:s0:f0:c2:NOZZLE REMOVED";
         message += String.format(",t:23:s2:f1:c1:Gallons꞉ %d", Controller.getGasAmount());
-        message += String.format("t:45:s2:f1:c1:Price꞉ $%.2f", Controller.getCurPrice());
-        device.send(new Message("t:67:s1:f1:c1:Re-insert to resume or FINISH."));
-        device.send(new Message("b:9:x,t:89:s2:f2:c0:|FINISH"));
+        message += String.format(",t:45:s2:f1:c1:Price꞉ $%.2f", Controller.getCurPrice());
+        message += ",t:67:s1:f1:c1:Re-insert to resume or FINISH.";
+        message += ",b:9:x,t:89:s2:f2:c0:|FINISH";
+        device.send(new Message(message));
     }
 
 
-    private void attachHose() {
+    private static void attachHose() {
         device.send(new Message("t:01:s0:f0:c2:PLEASE ATTACH THE HOSE"));
     }
 
-    private void fueling() {
+    private static void fueling() {
         String message = "";
         if (lastState != OFF && lastState != STANDBY) {
             message += "t:01:s0:f0:c2:PUMPING IN PROGRESS";
@@ -63,21 +78,21 @@ public class Display {
     }
 
     //todo: remove these if statements when displayGUI is fixed
-    private void pumpUnavailable() {
+    private static void pumpUnavailable() {
         if (lastState != OFF && lastState != STANDBY) {
             device.send(new Message("t:01:s0:f0:c2:Pump Currently Unavailable"));
         }
         lastState = STANDBY;
     }
 
-    private void welcome() {
+    private static void welcome() {
         if (lastState != IDLE) {
             device.send(new Message("t:01:s0:f0:c2:WELCOME!,t:45:s1:f1:c1:Please tap your credit card or phone's digital card to begin."));
         }
         lastState = IDLE;
     }
 
-    private void authorizing(){
+    private static void authorizing(){
         if (lastState != AUTHORIZING) {
             device.send(new Message("t:01:s0:f0:c2:Authorizing payment...,t:45:s1:f1:c1:Please Wait"));
         }
@@ -87,7 +102,7 @@ public class Display {
         lastState = AUTHORIZING;
     }
 
-    private void fuelSelect() {
+    private static void fuelSelect() {
         //todo show the correct screen
         if (lastState != SELECTION) {
             Message options = optionsDisplayable(Controller.getNewPriceList());
@@ -100,7 +115,7 @@ public class Display {
 
     }
 
-    private Message optionsDisplayable(ArrayList<Gas> options) {
+    private static Message optionsDisplayable(ArrayList<Gas> options) {
         String result = "t:01:s0:f0:c2:SELECT YOUR GAS TYPE,";
         int position = 2;
         for(Gas cur: options) {
@@ -111,7 +126,7 @@ public class Display {
         return new Message(result);
     }
 
-    private void cardDeclined() {
+    private static void cardDeclined() {
         if (lastState != DECLINED) {
             device.send(new Message("t:01:s0:f0:c2:Show the declined screen:45:s1:f1:c1:DECLINED!"));
         }
